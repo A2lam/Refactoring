2a195664b5f92373e0341dcbd58f66c9103e1953
HADJI Mohamed Allam <mohamedallam31@hotmail.com>
2019-06-28 17:09:30 +0200

    Création de la 2e classe téléphone

--- src/Telephone.java
+++ src/Telephone.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+public class Telephone
+{
+    private String officeAreaCode;
+    private String number;
 
-public class Telephone {
+    public Telephone() {}
+
+    public Telephone(String officeAreaCode, String number)
+    {
+        this.officeAreaCode = officeAreaCode;
+        this.number = number;
+    }
+
+    public String getOfficeAreaCode()
+    {
+        return officeAreaCode;
+    }
+
+    public void setOfficeAreaCode(String officeAreaCode)
+    {
+        this.officeAreaCode = officeAreaCode;
+    }
+
+    public String getNumber()
+    {
+        return number;
+    }
+
+    public void setNumber(String number)
+    {
+        this.number = number;
+    }
+
+    public String getTelephone()
+    {
+        return this.officeAreaCode + " " + this.number;
+    }
 }
